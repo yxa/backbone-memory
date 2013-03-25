ed6d28b45409c88633a274b13b6ed1c9956a796a
torbjorn josefsson <torbjorn.josefsson@gmail.com>
2013-03-25 20:27:31 +0100

    init commit

---
+++ backbone.memory.test.js
@@ -0,0 +1,10 @@
+suite('Backbone Memory Sync Adapter', function() {
+    test('backbone should be present', function() {
+      assert.ok(Backbone);
+    });
+
+    test('jquery should be present', function() {
+      assert.ok($);
+    });
+
+});
